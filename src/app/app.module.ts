cb354e119f901cd6539fc30a3664760b9f68712f
ilyaaleks <ilya.aleks.2011@gmail.com>
2021-05-09 15:53:49 +0300

    Prepare the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './components/registration/registration.component';
+import {StompService} from '@stomp/ng2-stompjs';
+import {StompConfig} from '@stomp/stompjs';
+import {stompConfig} from './config/stomp-config';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [StompService,
+    {
+      provide: StompConfig,
+      useValue: stompConfig
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/web-socket-config.ts
@@ -0,0 +1,4 @@
+export class WebSocketConfig {
+  public static uri: string = 'ws://localhost:8080/testws';
+  public static topic: string = '/topic/hello';
+}
