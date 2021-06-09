744d5befd7719bccfe1dc777cb409abc7d5dcef6
ilyaaleks <ilya.aleks.2011@gmail.com>
2021-06-09 08:35:35 +0300

    Fix url of service

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -2,24 +2,25 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Comment} from '../models/post/comment';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
-
+  baseUrl = environment.baseUrl;
   constructor(private httpClient: HttpClient) {
   }
 
   public get(postId: string): Observable<Comment[]> {
-    return this.httpClient.get<Comment[]>('/api/comments/' + postId);
+    return this.httpClient.get<Comment[]>(this.baseUrl +'/api/comments/' + postId);
   }
 
   public saveComment(comment: Comment): Observable<Comment> {
-    return this.httpClient.post<Comment>('/api/comments', comment);
+    return this.httpClient.post<Comment>(this.baseUrl +'/api/comments', comment);
   }
 
   public deleteComment(postId: string): Observable<Comment> {
-    return this.httpClient.delete<Comment>('/api/comments/' + postId);
+    return this.httpClient.delete<Comment>(this.baseUrl +'/api/comments/' + postId);
   }
 }
