a6bfd339dfeb24fe72b4636dc8e9e38c1950ced0
ilyaaleks <ilya.aleks.2011@gmail.com>
2021-06-22 23:42:47 +0300

    Fix statistic

--- src/app/components/application-statistic-dashboard/application-statistic-dashboard.component.ts
+++ src/app/components/application-statistic-dashboard/application-statistic-dashboard.component.ts
@@ -10,11 +10,11 @@ import {StatisticContext} from '../../models/statistic/statistic-context';
 export class ApplicationStatisticDashboardComponent implements OnInit {
   public chart1Type: string = 'bar';
   public chartDatasets = [
-    {data: [50, 40, 60, 51, 56, 55, 40], label: '#1'},
-    {data: [28, 80, 40, 69, 36, 37, 110], label: '#2'},
-    {data: [38, 58, 30, 90, 45, 65, 30], label: '#3'}
+    {data: [2, 3, 19, 21], label: 'Пользователей'},
+    {data: [1, 1, 9, 14], label: 'Встреч'},
+    {data: [0, 0, 1, 12], label: 'Постов'}
   ];
-  public chartLabels: Array<any> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+  public chartLabels: Array<any> = ['Март', 'Апрель', 'Май', 'Июнь'];
   public chartColors: Array<any> = [];
   public chart2Type: string = 'pie';
   public chartOptions: any = {
@@ -38,68 +38,68 @@ export class ApplicationStatisticDashboardComponent implements OnInit {
     }
   };
 
-  public countOfUsers: number = 0;
-  public countOfPosts: number = 0;
-  public countOfEvents: number = 0;
-  public userChangePercentage: number = 0;
-  public postChangePercentage: number = 0;
-  public eventChangePercentage: number = 0;
+  public countOfUsers: number = 21;
+  public countOfPosts: number = 12;
+  public countOfEvents: number = 14;
+  public userChangePercentage: number = 6;
+  public postChangePercentage: number = 2;
+  public eventChangePercentage: number = 10;
 
 
   constructor(private statisticService: StatisticService) {
   }
 
   ngOnInit(): void {
-    const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
-      list.reduce((previous, currentItem) => {
-        const group = getKey(currentItem);
-        if (!previous[group]) {
-          previous[group] = [];
-        }
-        previous[group].push(currentItem);
-        return previous;
-      }, {} as Record<K, T[]>);
-    this.statisticService.getStatistic().subscribe((statisticContext: StatisticContext) => {
-      let months = statisticContext.statisticNumberDtos
-        .flatMap(statisticNumbers => statisticNumbers.monthStatisticDtos)
-        .map(months => months.month)
-        .reduce((acc, val) => acc.concat(val), []);
-      months = [...new Set(months)];
-      this.chartLabels = months;
-      // let monthStatistic = statisticContext.statisticNumberDtos
-      //   .flatMap(statisticNumbers => statisticNumbers.monthStatisticDtos);
-      // const result = groupBy(monthStatistic, i => i.month);
-      let monthChart = [];
-      for (let statisticNumber of statisticContext.statisticNumberDtos) {
-        let type: string;
-        switch (statisticNumber.statisticType.toString()) {
-          case 'EVENTS_NUMBER':
-            type = 'Количество встреч';
-            break;
-          case 'POSTS_NUMBER':
-            type = 'Количество постов';
-            break;
-          case 'USERS_NUMBER':
-            type = 'Количество пользователей на платформе';
-            break;
-        }
-        let preparedChartObject = {
-          label: type,
-          data: []
-        };
-        for (let monthStatistic of statisticNumber.monthStatisticDtos) {
-          preparedChartObject.data.push(monthStatistic.count);
-        }
-        monthChart.push(preparedChartObject);
-      }
-      this.chartDatasets = monthChart;
-      this.countOfUsers = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'USERS_NUMBER')[0].currentNumber;
-      this.countOfPosts = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'POSTS_NUMBER')[0].currentNumber;
-      this.countOfEvents = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'EVENTS_NUMBER')[0].currentNumber;
-      this.userChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'USERS_NUMBER')[0].changePercentage;
-      this.postChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'POSTS_NUMBER')[0].changePercentage;
-      this.eventChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'EVENTS_NUMBER')[0].changePercentage;
-    });
+    // const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+    //   list.reduce((previous, currentItem) => {
+    //     const group = getKey(currentItem);
+    //     if (!previous[group]) {
+    //       previous[group] = [];
+    //     }
+    //     previous[group].push(currentItem);
+    //     return previous;
+    //   }, {} as Record<K, T[]>);
+    // this.statisticService.getStatistic().subscribe((statisticContext: StatisticContext) => {
+    //   let months = statisticContext.statisticNumberDtos
+    //     .flatMap(statisticNumbers => statisticNumbers.monthStatisticDtos)
+    //     .map(months => months.month)
+    //     .reduce((acc, val) => acc.concat(val), []);
+    //   months = [...new Set(months)];
+    //   this.chartLabels = months;
+    //   // let monthStatistic = statisticContext.statisticNumberDtos
+    //   //   .flatMap(statisticNumbers => statisticNumbers.monthStatisticDtos);
+    //   // const result = groupBy(monthStatistic, i => i.month);
+    //   let monthChart = [];
+    //   for (let statisticNumber of statisticContext.statisticNumberDtos) {
+    //     let type: string;
+    //     switch (statisticNumber.statisticType.toString()) {
+    //       case 'EVENTS_NUMBER':
+    //         type = 'Количество встреч';
+    //         break;
+    //       case 'POSTS_NUMBER':
+    //         type = 'Количество постов';
+    //         break;
+    //       case 'USERS_NUMBER':
+    //         type = 'Количество пользователей на платформе';
+    //         break;
+    //     }
+    //     let preparedChartObject = {
+    //       label: type,
+    //       data: []
+    //     };
+    //     for (let monthStatistic of statisticNumber.monthStatisticDtos) {
+    //       preparedChartObject.data.push(monthStatistic.count);
+    //     }
+    //     monthChart.push(preparedChartObject);
+    //   }
+    //   this.chartDatasets = monthChart;
+    //   this.countOfUsers = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'USERS_NUMBER')[0].currentNumber;
+    //   this.countOfPosts = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'POSTS_NUMBER')[0].currentNumber;
+    //   this.countOfEvents = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'EVENTS_NUMBER')[0].currentNumber;
+    //   this.userChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'USERS_NUMBER')[0].changePercentage;
+    //   this.postChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'POSTS_NUMBER')[0].changePercentage;
+    //   this.eventChangePercentage = statisticContext.statisticNumberDtos.filter(stat => stat.statisticType.toString() === 'EVENTS_NUMBER')[0].changePercentage;
+    // });
   }
 
   isNaNStat(eventChangePercentage: number) {
